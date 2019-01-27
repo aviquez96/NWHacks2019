aa7c9127b698151512aea99a981b8e93e2d63542
Adrian Viquez <aviquez96@gmail.com>
2019-01-27 03:09:15 -0800

    Transfering App to its own

--- FRNDZ/Components/Common/index.js
+++
@@ -1,7 +0,0 @@
-export * from './Button';
-export * from './Header';
-export * from './CardSection';
-export * from './Card';
-export * from './Input';
-export * from './Spinner';
-export * from './InputVertical';
