5bd9a736f281b7b2eb8a8867e25c07c98d842de0
Adrian Viquez <aviquez96@gmail.com>
2019-01-26 22:41:45 -0800

    try for keys

---
+++ frndz_web/src/Components/Firebase/firebase.js
@@ -0,0 +1,10 @@
+import * as Constants from '../../Constants/firebase'
+
+const config = {
+    apiKey: Constants.API_KEY,
+    authDomain: Constants.AUTH_DOMAIN,
+    databaseURL: Constants.DATABASE_URL,
+    projectId: Constants.PROJECT_ID,
+    storageBucket: Constants.STORAGE_BUCKET,
+    messagingSenderId: Constants.MESSAGING_SENDER_ID,
+};
